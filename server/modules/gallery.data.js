d10486bdcd4d9e31b61c7bf657c09f05286e53a9
Chloe Everson <chloeliz93@gmail.com>
2021-05-02 13:21:07 -0500

    updated app.js for gallery list component

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/bwca.jpg', title: 'BWCA', description: 'Fall trip with Dad 2020' , likes: 0 },
+    { id: 2, path: 'images/gma-and-dad.JPG', title: 'Dad and Grandma', description: 'Easter reunion, a wonderful day' , likes: 0 },
+    { id: 3, path: 'images/calhoun-sunset.jpg', title: 'Sunset', description: 'Bde Maka Ska 2021' , likes: 0 },
+    { id: 4, path: 'images/fam.jpg', title: 'BFFs', description: 'Last gathering with my squad before I went off to travel' , likes: 0 },
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList'
+import axios from 'axios'
+
+
 
 function App() {
-    return (
+  let [galleryList, setGalleryList] = useState([]);
+
+  //on load get images
+  useEffect(() => {
+    getImages()
+  }, [])
+
+  const getImages = () => {
+    axios.get('/gallery')
+      .then(response => {
+        setGalleryList(response.data)
+      })
+      .catch(err => {
+        alert('error getting images');
+        console.log(err);
+      })
+  }
+
+
+  return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList galleryList={galleryList} />
+        <img src="images/bwca.jpg"/>
       </div>
     );
 }
